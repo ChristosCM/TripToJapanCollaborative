cf61a4f89c11c9c5ed54fb2dcf723980e21875c7
Gabrielle Black <gabirose12@gmail.com>
2017-02-24 12:58:52 +0000

    123

---
+++ planning.py
@@ -0,0 +1,7 @@
+users = [n for n in range(1,21)]
+travel = ["plane","train","boat","car"]
+purpose = ["Studies","Leisure","Business","Sport"]
+duration = [n for n in range(1,366)]
+cost = [n for n in range(500,5000)]
+group = [n for n in range(1,11)]
+
